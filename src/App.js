3eb3126ed287356073fb11007c0b8f59ded2784e
Kaveri <patelkaveri14@gmail.com>
2022-06-30 16:18:19 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import React,{useEffect}from "react";
 import './App.css';
 
+import Home from './Home';
+import Checkout from './Checkout';
+import Login from './Login';
+import {useStateValue} from "./StateProvider"
+import {auth} from './firebase';
+
+import {BrowserRouter as Router,Routes,Route,Navigate} from "react-router-dom";
+
+
+
 function App() {
+  const [{},dispatch]=useStateValue();
+
+  useEffect(()=>{
+    auth.onAuthStateChanged(authUser=>{
+      
+
+      if(authUser){
+      dispatch({
+        type:"SET_USER",
+        user:authUser
+
+      })
+      }
+      else{
+       dispatch({
+        type:"SET_USER",
+        user:null
+        
+       })}
+    })
+  },[])
   return (
+    <Router>
+      
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+      
+     
+      
+      <Route path="/checkout" element={<Checkout/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/" element={<Home/>}/>
+      
+      
+      
+     
+        </Routes>
+      
+      </div>
+      </Router>
   );
 }
 
